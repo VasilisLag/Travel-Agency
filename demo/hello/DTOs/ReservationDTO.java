4822f20a89ccddf9681c91529f7dfca97c804c0c
Vasileios Lagopoulos <135460360+VasilisLag@users.noreply.github.com>
2024-02-03 17:07:22 +0200

    Delete demo directory

--- demo/hello/DTOs/ReservationDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.hello.DTOs;
-
-public class ReservationDTO {
-	private Long citizen;
-	private Long excursion;
-	
-	public Long getCitizen() {return citizen;}
-	public Long getExcursion() {return excursion;}
-}
